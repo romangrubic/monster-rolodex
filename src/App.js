ea9cf2c0183bb4d6ab097ab16d76f614561c86ba
Roman Grubic <grubic.roman@gmail.com>
2020-07-04 20:36:59 +0100

    Added CSS for App and Index js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './components/card-list/card-list.component';
 import './App.css';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
     constructor() {
@@ -15,20 +16,22 @@ class App extends Component {
             .then((response) => response.json())
             .then((users) => this.setState({ monsters: users }));
     }
-
+    handleChange = (e) => {
+        this.setState({ searchField: e.target.value });
+    };
     render() {
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter((monster) =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
         return (
             <div className='App'>
-                <input
-                    type='search'
-                    placeholder='Search monsters'
-                    onChange={(e) =>
-                        this.setState({ searchField: e.target.value }, () =>
-                            console.log(this.state)
-                        )
-                    }
+                <h1>Monster Rolodex</h1>
+                <SearchBox
+                    placeholder='Search monster'
+                    handleChange={this.handleChange}
                 />
-                <CardList monsters={this.state.monsters} />
+                <CardList monsters={filteredMonsters} />
             </div>
         );
     }
