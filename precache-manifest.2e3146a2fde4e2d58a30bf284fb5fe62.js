8562f27ea443823142e9a7a7d4ff3f9670b7f875
Roman Grubic <grubic.roman@gmail.com>
2020-07-04 21:08:08 +0100

    Updates

---
+++ precache-manifest.2e3146a2fde4e2d58a30bf284fb5fe62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99079e4ecae75b5fb7ad6af88bda93d2",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e5d56cbc81b7289d1d62",
+    "url": "/monster-rolodex/static/css/main.513d26b6.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d56cbc81b7289d1d62",
+    "url": "/monster-rolodex/static/js/main.a28fbbb4.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
